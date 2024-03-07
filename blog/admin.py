fe6b6fda9e72a9fa0574a1211ef60859e4023e4f
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-03-07 21:19:58 +0000

    added summernote html

--- blog/admin.py
+++ blog/admin.py
@@ -5,19 +5,17 @@ from django_summernote.admin import SummernoteModelAdmin
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
-    list_display = ('title', 'slug', 'status')
-    search_fields = ['title']
-    list_filter = ('status',)
-    prepopulated_fields = {'slug': ('title',)}
+    list_display = ('content','author')
+    search_fields = ['content']
+    
+
     summernote_fields = ('content',)
 
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     pass
 
-@admin.register(Post)
-class PostAdmin(admin.ModelAdmin):
-    list_display = ('author',)
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
