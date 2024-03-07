be15689774825bb10ae371853a8a28ec535e073f
Tadgh Mc Carthy <tadghmccarthy618@gmail.com>
2024-03-07 19:32:54 +0000

    url patterns

--- collegelink/urls.py
+++ collegelink/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('accounts/',include('allauth.urls')),
+    path('summernote/', include('django_summernote.urls')),
 ]
+
